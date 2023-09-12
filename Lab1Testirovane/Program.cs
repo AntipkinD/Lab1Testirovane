30b48122e2c07e0f29f15561dcfdad891098c745
mushroomvoodoo <normeeboy@gmail.com>
2023-09-12 11:54:18 +0300

    Добавьте файлы проекта.

---
+++ Lab1Testirovane/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void MinMax(ref int minA, ref int maxB)
+    {
+        int m = minA;
+        if (minA > maxB)
+        {
+            minA = maxB;
+            maxB = m;
+        }
+    }
+    private static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        MinMax(ref a, ref b);
+        Console.WriteLine($"A(min) = {a}, B(max) = {b}");
+    }
+}
